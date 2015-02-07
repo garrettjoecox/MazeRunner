1b6384c216f7a2103fd7e16e16541d5e454bb9b6
garrettjoecox <garrettjcox@gmail.com>
2015-02-07 14:04:56 -0800

    Attempting to fix background issue

--- js/app.js
+++ js/app.js
@@ -2,18 +2,13 @@
     'use strict';
     $(document).ready(function(){
 
-        var ran = Math.floor(Math.random()*10 + 1);
         var $clock = $(".clock")
-        var $img = $("img")
 
         /*---------------------------------------------------*/
 
         setTime();
         setInterval(setTime, 1000);
-        $img.attr("src", "backgrounds/" + ran.toString() + ".jpg")
-        $img.hide();
         $clock.hide();
-        $img.fadeIn(1000)
         $clock.delay(1000).fadeIn(1000)
 
         /*---------------------------------------------------*/
