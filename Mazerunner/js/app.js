11f10c69134bf4e9758969f9b7a07e882fc227be
garrettjoecox <garrettjcox@gmail.com>
2015-02-21 00:35:53 -0800

    Complete refactor, almost ready for multiple lines

--- Mazerunner/js/app.js
+++ Mazerunner/js/app.js
@@ -1,7 +1,4 @@
-/* Variables */
 var options = {
-    // Modify me to your liking!
-
     // Grid size
     grid: 25,
     // Toggles diagonal line drawing
@@ -22,105 +19,24 @@ var options = {
     // Boolean for line fade
     lineFade: true,
     // Amount of lines before fade
-    lineFadeDelay: 100,
+    lineFadeDelay: 50,
     // Background color
     bgColor: '#222'
 };
 
-var currentY = options.height/2;
-var currentX = options.width/2;
-var storage = [];
-var lineCount = 0;
-var directions = {
-    0: [0, options.grid],
-    1: [options.grid, 0],
-    2: [0, -options.grid],
-    3: [-options.grid, 0],
-    4: [options.grid, options.grid],
-    5: [-options.grid, -options.grid],
-    6: [options.grid, -options.grid],
-    7: [-options.grid, options.grid]
-};
+var svg = d3.select('svg')
+    .attr('height', options.height+'px')
+    .attr('width', options.width+'px')
+    .style('background', options.bgColor);
 
+var line1 = new Line(options);
 
-/* Logic */
-
-// Makes the canvas
-var svg = d3.select("svg")
-    .attr("height", options.height+"px")
-    .attr("width", options.width+"px")
-    .style("background", options.bgColor);
-
-// Clock to run functions
 setInterval(function(){
-    if (options.randomColor) randomColor();
-    populate();
-    update(storage);
+    line1.populate();
+    line1.draw(svg);
     setTime();
-    if (storage.length > options.lineFadeDelay && options.lineFade){
-        storage.shift();
-    }
 }, options.speed);
 
-
-/* Functions */
-
-function random(n){
-    return Math.floor(Math.random()*n);
-}
-
-function randomColor(){
-    var r = options.randomBrightness + random(256 - options.randomBrightness);
-    var g = options.randomBrightness + random(256 - options.randomBrightness);
-    var b = options.randomBrightness + random(256 - options.randomBrightness);
-    var color = "rgb(" + r + "," + g + "," + b + ")";
-    options.lineColor = color;
- }
-
-// Adds a stroke to the storage
-function add(x1, y1, x2, y2) {
-    if (arguments.length < 4) return;
-    storage.push({
-        'count': lineCount,
-        'x1': x1,
-        'y1': y1,
-        'x2': x2,
-        'y2': y2
-    });
-    lineCount++;
-}
-
-// Makes a new stroke to add
-function populate(){
-    var thing = options.diagonalLines ? 8 : 4;
-    var ran = Math.floor(Math.random()*thing);
-    var dir = directions[ran];
-    var newX = currentX + dir[0];
-    var newY = currentY + dir[1];
-    if (newX > options.width+options.grid || newY > options.height+options.grid || newX < 0-options.grid || newY < 0-options.grid) return;
-    add(currentX, currentY, newX, newY);
-    currentX = newX;
-    currentY = newY;
-}
-
-// Adds any new strokes in storage to canvas
-function update(data) {
-    var line = svg.selectAll("line")
-        .data(data, function(thing){return thing.count;});
-
-    line.enter().append("line")
-        .attr("x1", function(d){ return d.x1; })
-        .attr("y1", function(d){ return d.y1; })
-        .attr("x2", function(d){ return d.x2; })
-        .attr("y2", function(d){ return d.y2; })
-        .style("stroke", options.lineColor)
-        .style("stroke-width", "1")
-        .style("stroke-opacity", options.lineOpacity);
-
-    line.exit().remove();
-}
-
-// Updates clock
 function setTime(){
     var time = moment(Date.now()).format('h:mm');
     d3.select(".clock").text(time);
---
+++ Mazerunner/js/line.js
@@ -0,0 +1,58 @@
+function Line(options){
+    this.options = options;
+    this.currentX = options.height/2;
+    this.currentY = options.width/2;
+    this.storage = [];
+    this.lineCount = 0;
+    this.directions = {
+        0: [0, options.grid],
+        1: [options.grid, 0],
+        2: [0, -options.grid],
+        3: [-options.grid, 0],
+        4: [options.grid, options.grid],
+        5: [-options.grid, -options.grid],
+        6: [options.grid, -options.grid],
+        7: [-options.grid, options.grid]
+    };
+}
+
+Line.prototype.draw = function(svg){
+    if (this.options.randomColor){
+        r = this.options.randomBrightness + Math.floor(Math.random()*(256 - this.options.randomBrightness));
+        g = this.options.randomBrightness + Math.floor(Math.random()*(256 - this.options.randomBrightness));
+        b = this.options.randomBrightness + Math.floor(Math.random()*(256 - this.options.randomBrightness));
+        this.options.lineColor = "rgb(" + r + "," + g + "," + b + ")";
+    }
+    var line = svg.selectAll('line')
+        .data(this.storage, function(d){ return d.id; });
+    line.enter().append('line')
+        .attr('x1', function(d){ return d.x1; })
+        .attr('y1', function(d){ return d.y1; })
+        .attr('x2', function(d){ return d.x2; })
+        .attr('y2', function(d){ return d.y2; })
+        .style('stroke', this.options.lineColor)
+        .style('stroke-width', '1')
+        .style('stroke-opacity', options.lineOpacity);
+    line.exit().remove();
+};
+
+Line.prototype.populate = function(){
+    var type = this.options.diagonalLines ? 8 : 4;
+    var rand = this.directions[Math.floor(Math.random()*type)];
+    var newX = this.currentX + rand[0];
+    var newY = this.currentY + rand[1];
+    if (newX > this.options.width+this.options.grid || newY > this.options.height+this.options.grid || newX < 0-this.options.grid || newY < 0-this.options.grid) return;
+    this.storage.push({
+        id: this.lineCount,
+        x1: this.currentX,
+        y1: this.currentY,
+        x2: newX,
+        y2: newY
+    });
+    this.currentX = newX;
+    this.currentY = newY;
+    this.lineCount++;
+    if (this.storage.length > this.options.lineFadeDelay && this.options.lineFade){
+        this.storage.shift();
+    }
+};
